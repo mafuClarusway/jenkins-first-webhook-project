467a906d6f91d92603b4eb66743429276004fd2e
mafuClarusway <mayfult@gmail.com>
2022-09-20 11:24:31 +0000

    java testing 3

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java xoxo");
+        System.out.println("Hello from Java xoxo hihi");
     }
 }
